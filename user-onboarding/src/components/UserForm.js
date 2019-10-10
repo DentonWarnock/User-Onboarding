f727bc9ce2f250d2f7d7f0340e529d562d2c711b
DentonWarnock <dentonwarnock@gmail.com>
2019-10-10 11:56:21 -0700

    All form fields added. Need some styling for readability.

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -17,6 +17,23 @@ function UserForm(props) {
           {props.touched.name && props.errors.name && (
             <p className="error">{props.errors.name}</p>
           )}
+          <Field type="email" name="email" placeholder="Email Address" />
+          {props.touched.email && props.errors.email && (
+            <p className="error">{props.errors.email}</p>
+          )}
+          <Field type="password" name="password" placeholder="Password" />
+          {props.touched.password && props.errors.password && (
+            <p className="error">{props.errors.password}</p>
+          )}
+          <label className="checkbox-container">
+            <Field 
+              type="checkbox"
+              name="TOS"
+              checked={props.values.TOS}
+              />
+              Accept Terms of Service
+          </label>
+          <button type="submit">Submit</button>
         </Form>
       </div>   
   )
