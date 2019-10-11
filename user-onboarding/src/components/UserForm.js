05e6c26fbfaefdef128dadfb285c803c9039fd59
DentonWarnock <dentonwarnock@gmail.com>
2019-10-10 19:29:59 -0700

    TOS validation working properly and styling updates.

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -15,24 +15,26 @@ function UserForm(props) {
   return (
     <div className="user-form">
         <Form>
-          <Field type="text" name="name" placeholder="Name" />
+          <Field type="text" name="name" placeholder="  Name" />
           {props.touched.name && props.errors.name && (
             <p className="error">{props.errors.name}</p>
           )}
-          <Field type="email" name="email" placeholder="Email Address" />
+          <Field type="email" name="email" placeholder="  Email Address" />
           {props.touched.email && props.errors.email && (
             <p className="error">{props.errors.email}</p>
           )}
-          <Field type="password" name="password" placeholder="Password" />
+          <Field type="password" name="password" placeholder="  Password" />
           {props.touched.password && props.errors.password && (
             <p className="error">{props.errors.password}</p>
           )}
           <label className="checkbox-container">
             <Field 
               type="checkbox"
-              name="TOS"
-              checked={props.values.TOS}
+              name="tos"
+              checked={props.values.tos}
               />
+              {props.touched.tos && props.errors.tos && 
+                (<p className="error">{props.errors.tos}</p>)}
               Accept Terms of Service
           </label>
           <button type="submit">Submit</button>
@@ -45,6 +47,9 @@ const myMapPropsToValues = props => {
   console.log("myMapPropsToValues", props)
   const returnObj = {
     name: props.name || "",
+    email: props.email || "",
+    password: props.password || "",
+    tos: props.tos || false
   };
   return returnObj;
 };
@@ -61,7 +66,9 @@ const yupSchema = Yup.object().shape({
     .min(6, 'Password must be at least 6 characters')
     .max(20, "Password is too long")
     .required('Password is required'),
-  TOS: Yup.boolean('You must agree to the Terms of Service')
+  tos: Yup.bool()
+  .oneOf([true], 'You must agree to the Terms of Service')
+  // .required('You must agree to the Terms of Service')  
 })
 
 const formikObj = {
